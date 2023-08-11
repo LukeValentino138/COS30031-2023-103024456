19414b5a9a6e0b6064a1f5628131571cac8a4e31
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-08-11 20:57:33 +1000

    Player Movement
    Created and tested player movement around grid

--- 03 - Spike - Gridworld/gridworld.cpp
+++ 03 - Spike - Gridworld/gridworld.cpp
@@ -1,34 +1,87 @@
 #include <iostream>
+using namespace std;
 
-std::string mapArray[5][5]=
-{
-  {"#","#","#","#","#"},
-  {"#","D"," ", "G","#"},
-  {"#"," "," "," ","#"},
-  {"#"," ","S", " ","#"},
-  {"#","#","#","#","#"}  
+const int arraySize = 8;
+
+struct Player {
+    int x;
+    int y;
 };
 
+string mapArray[arraySize][arraySize] = {
+    {"#", "#", "#", "#", "#", "#", "#", "#"},
+    {"#", "G", " ", "D", "#", "D", " ", "#"},
+    {"#", " ", " ", " ", "#", " ", " ", "#"},
+    {"#", "#", "#", " ", "#", " ", "D", "#"},
+    {"#", " ", " ", " ", "#", " ", " ", "#"},
+    {"#", " ", "#", "#", "#", "#", " ", "#"},
+    {"#", " ", " ", " ", " ", " ", " ", "#"},
+    {"#", "#", "S", "#", "#", "#", "#", "#"}  
+};
 
-void outputMap()
+void outputMap(Player player)
 {
-for (int i = 0; i < 5; i++)
+    for (int i = 0; i < arraySize; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < arraySize; j++)
         {
-            std::cout << mapArray[i][j];
+            if (i == player.y && j == player.x) {
+                cout << "P";
+            } else {
+                cout << mapArray[i][j];
+            }
         }
-        std::cout << std::endl; // Move to next line after row is done
+        cout << endl;
     }
 }
 
 
-int main()
+Player playerMovement(Player player)
 {
-    outputMap();
- 
-   return 0;
+    char dir;
+
+    cout << "Choose a direction to travel (N,E,S,W) or Quit (Q): ";
+    cin >> dir;
+    dir = toupper(dir);
+
+    switch (dir) {
+        case 'N':
+            if (player.y > 0) {
+                player.y--;
+            }
+            break;
+        case 'S':
+            if (player.y < arraySize - 1) {
+                player.y++;
+            }
+            break;
+        case 'W':
+            if (player.x > 0) {
+                player.x--;
+            }
+            break;
+        case 'E':
+            if (player.x < arraySize - 1) {
+                player.x++;
+            }
+            break;
+        case 'Q':
+            exit(0);
+        default:
+            cout << "Invalid direction." << endl;
+    }
+    return player;
 }
 
+int main()
+{
+    Player player = {2, 7};
 
+    while (true) {
+        outputMap(player);
+        cout << endl;
+        player = playerMovement(player);
+    }
 
+    return 0;
+}
