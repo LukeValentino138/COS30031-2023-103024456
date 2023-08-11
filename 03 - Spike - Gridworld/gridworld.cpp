74438e47c85c5f3d033f8344e1a610fdfed337bd
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-08-11 17:19:49 +1000

    String array functionality
    created and tested initial string array

--- 03 - Spike - Gridworld/gridworld.cpp
+++ 03 - Spike - Gridworld/gridworld.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+std::string mapArray[3][3]=
+{
+  {"D","", "G"},
+  {"","",""},
+  {"","S", ""}  
+};
+
+
 int main()
 {
-    std::cout << "Git Testing" << std::endl;
+    std::cout << mapArray[2][1] << std::endl;
     return 0;
+
 }
+
+
+
