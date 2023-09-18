98036dd09f6cac678afeef0d92833b8f1836a08e
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-09-18 17:44:45 +1000

    Multiple Function Measurement

--- 07 - Spike - Performance Measurement/task07.cpp
+++ 07 - Spike - Performance Measurement/task07.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 using namespace std::chrono;
 
-void single_measurement(int size)
+void single_func(int size)
 {
     cout << "Single Function Measurement" << endl;
     int total;
@@ -28,7 +28,39 @@ void single_measurement(int size)
     cout << "time: " << diff.count() << ", time/int: " << diff.count()/size << endl;
 }
 
+void multiple_func(int size, int executions)
+{
+    cout << "Multiple Function Measurement" << endl;
+    int total;
+    vector<int> v(size, 50);
+
+    //start time
+    auto start = steady_clock::now();
+
+    //loop
+    for (auto i = 1; i <= executions; i++){
+        //perform measureable action
+        total = accumulate(v.begin(), v.end(), 0u); 
+
+        //end time
+        auto end = steady_clock::now();
+
+        //calculate time
+        duration<double> diff = end - start;
+
+        //print time and time per int
+        cout << "Execution: " << i;
+        cout << ", Time: " << diff.count() << "s, time/int: " << diff.count()/size << "s/int" << endl;
+    }
+
+   
+
+    
+}
+
+
 int main()
 {
-    single_measurement(100000);
+    single_func(100000);
+    multiple_func(100000, 10);
 }
