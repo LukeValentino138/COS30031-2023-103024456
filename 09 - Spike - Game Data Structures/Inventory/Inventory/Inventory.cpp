474a967ddd6d0ffc38e31c346c37089f59797a76
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-10-07 18:15:00 +1100

    Created Remove function
    created remove function

--- 09 - Spike - Game Data Structures/Inventory/Inventory/Inventory.cpp
+++ 09 - Spike - Game Data Structures/Inventory/Inventory/Inventory.cpp
@@ -11,6 +11,17 @@ public:
         items[itemName] += quantity;
     }
 
+    bool removeItem(const string& itemName, int quantity = 1) {
+        auto it = items.find(itemName);
+
+        it->second -= quantity;
+
+        if (it->second <= 0) {
+            items.erase(it);
+        }
+        return true;
+    }
+
     void printInventory() const {
         for (const auto& pair : items) {
             cout << pair.first << ": " << pair.second << endl;
@@ -24,7 +35,21 @@ int main() {
     playerInventory.addItem("Sword", 1);
     playerInventory.addItem("Bread", 2);
 
+    std::cout << "Initial Inventory:" << std::endl;
     playerInventory.printInventory();
+
+    std::cout << "\nInventory after removing a sword and bread:" << std::endl;
+
+    playerInventory.removeItem("Sword", 1);
+    playerInventory.removeItem("Bread", 1);
+
+    playerInventory.printInventory();
+
+    std::cout << "\nInventory after removing a more bread:" << std::endl;
+    playerInventory.removeItem("Bread", 10);
+
+    playerInventory.printInventory();
+
 
     return 0;
 }
