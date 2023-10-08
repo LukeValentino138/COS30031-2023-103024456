c7ab64253654dbeea596a2a349a21c327ba857de
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-10-08 20:14:09 +1100

    Created Open/Write and read functions

--- 10 - Lab - File Input Output/Lab-FIleInputOutput/Lab-FIleInputOutput/BinaryFileOutputInput.cpp
+++ 10 - Lab - File Input Output/Lab-FIleInputOutput/Lab-FIleInputOutput/BinaryFileOutputInput.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -8,6 +9,8 @@ private:
 	char _character;
 	float _float;
 
+	std::string filename = "test1.bin";
+
 public:
 
 	Basic(int integer, char character, float f) {
@@ -16,9 +19,46 @@ public:
 		_float = f;
 	}
 
+	void openFile() {
+		std::fstream file(filename, std::ios::in | std::ios::out | std::ios::binary | std::ios::app);
+
+		if (!file) {
+			std::cerr << "Error, cannot open file" << filename << std::endl;
+			return;
+		}
+
+		file.write(reinterpret_cast<char*>(&_integer), sizeof(_integer));
+		file.write(&_character, sizeof(_character));
+		file.write(reinterpret_cast<char*>(&_float), sizeof(_float));
+
+		file.close();
+	}
+
+	void readFile() {
+		std::fstream file(filename, std::ios::in | std::ios::binary);
+
+		if (!file) {
+			std::cerr << "Error, cannot open file << filename << std::endl;
+			return;
+		}
+
+		file.read(reinterpret_cast<char*>(&_integer), sizeof(_integer));
+		file.read(&_character, sizeof(_character));
+		file.read(reinterpret_cast<char*>(&_float), sizeof(_float));
+
+		file.close();
+
+		std::cout << "Verify file data:" << std::endl;
+		std::cout << "Integer: " << _integer << std::endl;
+		std::cout << "Character: " << _character << std::endl;
+		std::cout << "Float: " << _float << std::endl;
+	}
+
 };
 
 int main() {
 	Basic basic(1, 'a', 2.0f);
+	basic.readFile();
+
 	return 0;
 }
