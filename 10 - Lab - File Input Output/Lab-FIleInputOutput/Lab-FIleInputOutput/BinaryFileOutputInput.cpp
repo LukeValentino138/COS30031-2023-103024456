9680928cb3cdfc4fcbbed16942696b1f4f18df28
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-10-08 19:32:03 +1100

    Fixed small issue

--- 10 - Lab - File Input Output/Lab-FIleInputOutput/Lab-FIleInputOutput/BinaryFileOutputInput.cpp
+++ 10 - Lab - File Input Output/Lab-FIleInputOutput/Lab-FIleInputOutput/BinaryFileOutputInput.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
 class Basic {
 private:
-
-public:
 	int _integer;
 	char _character;
 	float _float;
+
+public:
 
 	Basic(int integer, char character, float f) {
 		_integer = integer;
 		_character = character;
 		_float = f;
 	}
+
 };
 
 int main() {
-	Basic(1, 'a', 2.0f);
+	Basic basic(1, 'a', 2.0f);
 	return 0;
 }
