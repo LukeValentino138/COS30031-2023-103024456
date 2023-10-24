96e6b17ab9a9177ca36b31d1ee984edb541bd11c
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-10-25 01:49:27 +1100

    Created ActionComponent.h
    Created ActionComponent.h. Will be used for actions such as shoot, open, etc.

---
+++ 13 - Spike - Composite and Component Patterns/Task13/Task13/ActionComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Component.h"
+#include <string>
+
+class ActionComponent : public Component {
+public:
+    virtual void execute() = 0;
+    virtual std::string getName() const = 0;
+};
