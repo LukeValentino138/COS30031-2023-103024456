f77c9023cbed4ccc7b899b91d5ebb2d10438ba1a
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-10-25 02:02:32 +1100

    Update

--- 13 - Spike - Composite and Component Patterns/Task13/Task13/Player.h
+++ 13 - Spike - Composite and Component Patterns/Task13/Task13/Player.h
@@ -12,17 +12,17 @@ public:
     Player() : currentLocation(nullptr) {}
 
     // Gets the current location of the player.
-    inline Vertex* getCurrentLocation() const {
+    Vertex* getCurrentLocation() const {
         return currentLocation;
     }
 
     // Sets the current location of the player.
-    inline void setCurrentLocation(Vertex* location) {
+    void setCurrentLocation(Vertex* location) {
         currentLocation = location;
     }
 
     // Gets a reference to the player's inventory.
-    inline Inventory& getInventory() {
+    Inventory& getInventory() {
         return inventory;
     }
 };
