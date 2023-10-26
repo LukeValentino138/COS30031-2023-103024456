2f1e34051896abe46ef025f68535420f855434ca
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2023-10-26 19:23:38 +1100

    Add Background
    added background to window

--- 17 - Spike - Sprites and Graphics/Task17/Task17/main.cpp
+++ 17 - Spike - Sprites and Graphics/Task17/Task17/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
 
 int main(int argc, char* argv[]) {
     // Initialize SDL
@@ -8,7 +9,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Create a window
+    // Create window
     SDL_Window* window = SDL_CreateWindow("Image Display",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -16,16 +17,53 @@ int main(int argc, char* argv[]) {
         600,  // height
         SDL_WINDOW_SHOWN);
 
+    // window check
     if (!window) {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    // Wait for 5 seconds
-    SDL_Delay(5000);
+    // Make renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Load image
+    SDL_Surface* bgSurface = IMG_Load("background.bmp");
+    if (!bgSurface) {
+        std::cerr << "Failed to load background image: " << IMG_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Texture* bgTexture = SDL_CreateTextureFromSurface(renderer, bgSurface);
+    SDL_FreeSurface(bgSurface);  
+
+    // Keep showing image until close
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Show iamge
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, bgTexture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
 
     // Cleanup
+    SDL_DestroyTexture(bgTexture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
